1ee59634f579cd2ac6310bf4d01e7fd6ea233052
6-MillionBees <BoettcherAr321@northwested.org>
2025-03-07 11:18:06 -0500

    working on it

--- config.py
+++ config.py
@@ -2,6 +2,8 @@
 # Insert date here
 # Insert title here
 
+from pygame import Rect
+
 # Screen size constants
 WIDTH = 500
 HEIGHT = 500
@@ -17,3 +19,18 @@ BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 BLUE = (0, 255, 0)
 GREEN = (0, 0, 255)
+
+
+FOREGROUND_RECTS = [
+  Rect(0, 292, 44, 208),
+  Rect(44, 258, 46, 242),
+  Rect(90, 398, 41, 102),
+  Rect(131, 308, 59, 192),
+  Rect(190, 355, 30, 155),
+  Rect(220, 397, 49, 103),
+  Rect(269, 367, 52, 133),
+  Rect(321, 374, 42, 126),
+  Rect(363, 267, 55, 233),
+  Rect(418, 247, 42, 253),
+  Rect(460, 316, 40, 184),
+]
--- main.py
+++ main.py
@@ -1,18 +1,11 @@
 # Arden Boettcher
-# Insert date here
-# Insert title here
+# 3/7/25
+# Fireworks Drawing
 
 import pygame
 import config
 pygame.init()
 
-
-# Setting up the window
-screen = pygame.display.set_mode((config.WIDTH, config.HEIGHT))
-pygame.display.set_caption("PLACEHOLDER")
-
-# Setting up the clock
-clock = pygame.time.Clock()
 
 # Event handling
 def main_events():
@@ -26,9 +19,20 @@ def main_events():
 
 # Main loop
 def main():
+
+  # Setting up the window
+  screen = pygame.display.set_mode((config.WIDTH, config.HEIGHT))
+  pygame.display.set_caption("PLACEHOLDER")
+
+  # Setting up the clock
+  clock = pygame.time.Clock()
+
   # The bool for the main loop
   running = True
-  
+
+  # A few misc things
+  # overlay = pygame.Surface()
+
   while running:
 
     # Call events / update running
@@ -36,6 +40,8 @@ def main():
 
     # Fills window
     screen.fill(config.WHITE)
+
+    draw_foreground(screen)
 
     # Updates the Display
     pygame.display.flip()
@@ -45,6 +51,14 @@ def main():
 
   # Close the pygame modules
   pygame.quit()
+
+
+def draw_foreground(surface):
+  for rect in config.FOREGROUND_RECTS:
+    pygame.draw.rect(surface, config.BLACK, rect)
+
+def draw_background(surface):
+  cloud = pygame.Surface(100, 70)
 
 
 # Calls the code
